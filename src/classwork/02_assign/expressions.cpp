c490d15c8cc363b8b0fa6ee68bffbd2ea15fd0e0
Salim Jemai <salim.jemai@g.austincc.edu>
2022-02-10 19:36:20 -0600

    hw-02-expressions corrected include header

--- src/classwork/02_assign/expressions.cpp
+++ src/classwork/02_assign/expressions.cpp
@@ -1,18 +1,11 @@
 //Write the include statement for types.h here
-#include "expressions.h"
 
 
 //write function prototype for multiply_numbers with two integer parameters num1, and num2
-double get_sales_tax_amount(double meal_amount)
-{
-	return meal_amount * (6.75/100);
-}
+
 
 //write function that will return the tip amount
-double get_tip_amount(double meal_amount, double tip_rate)
-{
-	return meal_amount * (tip_rate/100);
-}
+
 
 
 
